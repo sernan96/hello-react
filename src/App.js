a3a64dbb5581d88008600e189fc3f5bceaeb5aef
Hwang gend <ghkdtjsdks@naver.com>
2024-07-03 18:33:27 +0900

    7.13 props 배열로 받기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(){
+function Header(props){
   return (
     <header>
-        <h1><a href ="/">WEB</a></h1>
+        <h1><a href ="/">{props.title}</a></h1>
       </header>
   )
 }
-function Nav(){
+function Nav(props){
+  const lis = []
+  for(let i =0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
   return(
     <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-        </ol>
-      </nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
   )
 }
-function Article(){
+function Article(props){
   return(
     <article> 
-      <h2>Welcome</h2>
-      Hello, Web  
+      <h2>{props.title}</h2>
+      {props.body}  
     </article>
   )
 }
 //헤더 영역, 네비게이션 영역, 아티클 영역들로 나뉨
 function App() {
+  const topics =[
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+  ]
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
