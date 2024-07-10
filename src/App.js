7f8b4fa6812cdb547ee73871b7dc9475fcec4be9
Hwang gend <ghkdtjsdks@naver.com>
2024-07-10 19:48:23 +0900

    [UPDATE] state 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function Header(props){
   return (
@@ -18,7 +19,7 @@ function Nav(props){
     lis.push(<li key={t.id}>
       <a href={'/read/'+t.id} onClick = {event=>{
       event.preventDefault();
-      props.onChangeMode(t.id);
+      props.onChangeMode(Number(t.id));
     }}>{t.title}</a>
     </li>);
   }
@@ -40,20 +41,37 @@ function Article(props){
 }
 //헤더 영역, 네비게이션 영역, 아티클 영역들로 나뉨
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics =[
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
   ]
+  let content =null;
+  if(mode==='WELCOME'){
+    content = <Article title="Welcome" body="Hello, Web"></Article>;
+  }else if(mode ==='READ'){
+    let title, body =null;
+    for(let i =0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id===id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
     <div className="App">
-      <Header title="REACT" onChangeMode={function(){
-        alert('Header');
+      <Header title="REACT" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, Web"></Article>
+      {content}
     </div>
   );
 }
