2b18a9d0eef7b90a8154dc089debc7bfa6933bd2
Hwang gend <ghkdtjsdks@naver.com>
2024-07-13 19:56:35 +0900

    [UPDATE] 글 추가form (Create 구현)

--- src/App.js
+++ src/App.js
@@ -39,15 +39,31 @@ function Article(props){
     </article>
   )
 }
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type ="text" name="title" placeholder="title"/></p>
+      <p><textarea name ="body" placeholder="body"></textarea></p>
+      <p><input type ="submit" value="Create"/></p>
+    </form>
+  </article>
+}
 //헤더 영역, 네비게이션 영역, 아티클 영역들로 나뉨
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics =[
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
-  ]
+  ]);// useState로 감싸주어서 승격시켜줌
   let content =null;
   if(mode==='WELCOME'){
     content = <Article title="Welcome" body="Hello, Web"></Article>;
@@ -61,6 +77,14 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  }else if(mode ==='CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]; 
+      newTopics.push(newTopic);
+      //original data 건들 ㄴㄴ
+      setTopics(newTopics);
+    }}></Create>
   }
   return (
     <div className="App">
@@ -72,6 +96,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode("CREATE");
+      }}>Create</a>
     </div>
   );
 }
